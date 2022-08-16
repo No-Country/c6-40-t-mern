14857755197f59c3227478849b97c4f26929d101
Luca Hardmeier <hardmeierluca@gmail.com>
2022-08-16 13:22:17 -0300

    save into AWS S3 added into article.controller, article model has been modified and new img validation added.

--- backend/src/config/aws-s3.config.js
+++ backend/src/config/aws-s3.config.js
@@ -1,17 +1,12 @@
 const { S3Client } = require("@aws-sdk/client-s3");
-import dotenv from 'dotenv'
-import multer from 'multer';
-
-dotenv.config()
 
 const {
-    BUCKET_NAME: bucketName,
     BUCKET_REGION: region,
     ACCESS_KEY: accessKeyId,
     SECRET_ACCESS_KEY: secretAccessKey
 } = process.env
 
-exports.s3 = new S3Client({
+module.exports.s3 = new S3Client({
     credentials: {
         accessKeyId,
         secretAccessKey,
