ea8adb2fd45d3a093c5f0bbf16ae16f259145898
kenny <kenny.luque.t@uni.pe>
2022-08-18 18:04:16 -0500

    fetch test

--- frontend/pages/form.tsx
+++ frontend/pages/form.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+
 const Form = () => {
-    return (
-      <div>
-        <p className="mt-20">Aca el formulario............</p>
-      </div>
-    );
-  };
-  
-  export default Form;
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/api/v1/user/62fea6442b351f2d0bce250e')
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  return (
+    <div>
+      <p className="mt-20">Aca el formulario............</p>
+
+
+    </div>
+  );
+};
+
+export default Form;
