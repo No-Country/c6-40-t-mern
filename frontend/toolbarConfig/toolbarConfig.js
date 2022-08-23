5d71a75321febe4116f001ce481dbf0290224c87
ezexgonzalez <ezexgonzalez@outlook.com>
2022-08-23 00:09:13 -0300

    changes form

---
+++ frontend/toolbarConfig/toolbarConfig.js
@@ -0,0 +1,12 @@
+const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}],
+      ['link'],
+      ['clean'],  [{ 'color': [] }, { 'background': [] }],
+    ],
+}
+
+
+export default modules;
