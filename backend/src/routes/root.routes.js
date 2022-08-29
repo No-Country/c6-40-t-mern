83eee4e2d5690af6c66300c35c900edf0b243336
kenny <kenny.luque.t@uni.pe>
2022-08-29 16:23:16 -0500

    fix: prod render html root

--- backend/src/routes/root.routes.js
+++ backend/src/routes/root.routes.js
@@ -2,7 +2,33 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname })
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Blogg</title>
+  </head>
+  <body>
+    <h1>API Blogger</h1>
+    <table >
+      <thead>
+        <tr>
+          <th>Endpoint</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>/api/v1/user</td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+  </html>
+  `)
 })
 
 module.exports = router
