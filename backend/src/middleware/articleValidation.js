3291ad4376a36f231b0133b8d62918a82369bbe5
Jesus Cabrita <jesusarnaldo115@gmail.com>
2022-08-29 22:48:27 -0300

    front and back

--- backend/src/middleware/articleValidation.js
+++ backend/src/middleware/articleValidation.js
@@ -1,11 +1,11 @@
 const { validateArticle } = require('../models/articles')
 
 module.exports = (req, res, next) => {
-  req.body.tags = req.body.tags.split(",")
+  req.body.tags = req.body.tags.split(',')
 
   const { error } = validateArticle(req.body)
 
-  if (error) return res.status(400).send(error.details[0].message)
-  if (req.method === "POST" && !req.file) return res.status(400).send('You must send an image with the article')
+  if (error) return res.status(400).send(error)
+  if (req.method === 'POST' && !req.file) return res.status(400).send('You must send an image with the article')
   else next()
 }
